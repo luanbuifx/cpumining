5225858726a93ea41ed2f2c64a1103717a4f26ef
luanbuifx <luanbuifx@gmail.com>
2019-03-20 14:57:43 +0700

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -15,7 +15,6 @@ cmake .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin/ &&
 sudo sysctl -w vm.nr_hugepages=128 &&
-sudo cpulimit --exe "$cpuname" -l 300 &&
 sudo bash -c 'cat <<EOT >>/usr/local/src/build/bin/config.txt
 "call_timeout" : 10,
 "retry_time" : 30,
