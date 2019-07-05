0edf3412f4310db2516a60ffc48dc3d077ecd0ec
luanbuifx <luanbuifx@gmail.com>
2019-07-05 16:17:43 +0700

    Update lokitur.sh

--- lokitur.sh
+++ lokitur.sh
@@ -37,7 +37,7 @@ sudo bash -c 'cat <<EOT >>/usr/local/src/build/bin/pools.txt
 "pool_list": [
 	{
 		"pool_address": "lokiturtle.herominers.com:10521",
-		"wallet_address": "LZY6QfG3gYGWHiieqPfnZtAffUV3UR7ikemzgrPwV2G88tneNqmYYGbN6ebZF56DRPavF5YnvDDJSPazqbceABDw8Bh8kCy+1862048128",
+		"wallet_address": "LZY6QfG3gYGWHiieqPfnZtAffUV3UR7ikemzgrPwV2G88tneNqmYYGbN6ebZF56DRPavF5YnvDDJSPazqbceABDw8Bh8kCy+0000000000000000000000000000000000000000000000000000001862048128",
 		"rig_id": "rig_id",
 		"pool_password": "TRTLv2xgnXYJBqb7BuX2MwFZg42hfNDxrK9Y7kjZBNHuZRM17sJSEvQQK8BDhb3Nra48dvXydwZEjA7gWKNwLRJpE7Rw4DXjQqJ",
 		"use_nicehash": false,
