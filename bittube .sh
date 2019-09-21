519f96a6c6be813e071a029b2cf0e312f47bffca
luanbuifx <luanbuifx@gmail.com>
2019-09-21 22:03:52 +0700

    Create bittube .sh

---
+++ bittube .sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install cpulimit 
+sudo apt --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git libuv1-dev 
+mkdir /usr/local
+rm -r /usr/local/src
+mkdir /usr/local/src
+git clone https://github.com/fireice-uk/xmr-stak.git /usr/local/src &&
+a='nwmtjxvcxt-00' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+cd /usr/local/src
+mkdir build
+cd build 
+cmake .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+make install
+cd bin
+sudo sysctl -w vm.nr_hugepages=128 
+sudo bash -c 'cat <<EOT >>/usr/local/src/build/bin/config.txt
+"call_timeout" : 10,
+"retry_time" : 30,
+"giveup_limit" : 0,
+"verbose_level" : 3,
+"print_motd" : true,
+"h_print_time" : 60,
+"aes_override" : null,
+"use_slow_memory" : "warn",
+"tls_secure_algo" : true,
+"daemon_mode" : false,
+"flush_stdout" : false,
+"output_file" : "",
+"httpd_port" : 99,
+"http_login" : "",
+"http_pass" : "",
+"prefer_ipv4" : true,
+EOT
+' &&
+sudo bash -c 'cat <<EOT >>/usr/local/src/build/bin/pools.txt
+"pool_list" :
+[
+	{"pool_address" : "bittube.miner.rocks:5555", "wallet_address" : "bi1b95WYJRES7oBrvRo2eQV53ExLzFAzjKVM4wp9H9B6irCR6UuQxHf183XsJwemdoQm5PUHhQVwS67Hf5yUE7qg4SwbdmLusFV5LhT7vULQD", "rig_id" : "", "pool_password" : "w=Rig1", "use_nicehash" : false, "use_tls" : true, "tls_fingerprint" : "", "pool_weight" : 100 },
+	{"pool_address" : "bittube.miner.rocks:7777", "wallet_address" : "bi1b95WYJRES7oBrvRo2eQV53ExLzFAzjKVM4wp9H9B6irCR6UuQxHf183XsJwemdoQm5PUHhQVwS67Hf5yUE7qg4SwbdmLusFV5LhT7vULQD", "rig_id" : "", "pool_password" : "w=Rig1", "use_nicehash" : false, "use_tls" : true, "tls_fingerprint" : "", "pool_weight" : 1 },
+],
+"currency" : "bittube",
+
+EOT
+' &&
+sudo cp xmr-stak "$cpuname"
+rm xmr-stak
+echo $cpuname" is starting"
+cpulimit -l 395 ./"${cpuname}"
